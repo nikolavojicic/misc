feb8e871385b3c22098713ed56384a083e78ceb1
nikolavojicic <nikolavojicic@outlook.com>
2021-11-30 12:46:08 +0100

    adventofcode_2020_p01

---
+++ src/misc/adventofcode/2020/p01.clj
@@ -0,0 +1,45 @@
+(ns misc.adventofcode.2020.p01
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer [with-test is]]))
+
+
+(def +in1 [1721
+           979
+           366
+           299
+           675
+           1456])
+
+
+(def +in2 (->> "adventofcode_2020_p01_i01.txt"
+               io/resource io/reader line-seq
+               (mapv #(Long/parseLong %))))
+
+
+(with-test
+
+  (defn solve1
+    [input]
+    (first
+     (for [i1 input i2 input
+           :when (= 2020 (+ i1 i2))]
+       (* i1 i2))))
+
+  (is (= (solve1 +in1) 514579))
+  (is (= (solve1 +in2) 902451)))
+
+
+(with-test
+
+  (defn solve2
+    [input]
+    (first
+     (for [i1 input i2 input i3 input
+           :when (= 2020 (+ i1 i2 i3))]
+       (* i1 i2 i3))))
+
+  (is (= (solve2 +in1) 241861950))
+  (is (= (solve2 +in2) 85555470)))
+
+
+#_(clojure.test/run-tests *ns*)
